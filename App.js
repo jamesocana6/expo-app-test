caf7bc20ba4b026d3821b27c756f8a45c9ee3b54
James Ocana <ojam1996@gmail.com>
2023-01-06 04:46:42 -0500

    buttons and text

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text><h1>h1 in Text</h1></Text>
+      <Text>Just Text</Text>
+      <h1>Just h1</h1>
+      <Button
+        onPress={() => {
+          alert('You tapped the button!');
+        }}
+        title="Press Me"
+      />
       <StatusBar style="auto" />
     </View>
   );
