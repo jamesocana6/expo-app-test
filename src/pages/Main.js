b2fba80794ce16217fc05ffa453d5b6e929c9a56
James Ocana <ojam1996@gmail.com>
2023-01-09 03:15:03 -0500

    added icons to tabbar

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Profile from "./Profile";
 import Leaderboard from "./Leaderboard";
 import Home from "./Home";
+import { Feather } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,7 +12,16 @@ const Main = () => {
     return (
         <>
             <Tab.Navigator>
-                <Tab.Screen name="Home" component={Home} />
+                <Tab.Screen 
+                name="Home" 
+                component={Home} 
+                options={{
+                    tabBarLabel: 'Waffle',
+                    tabBarIcon: ({ color, size }) => (
+                        <Feather name="user" color={color} size={size} />
+                      )
+                  }}
+                />
                 <Tab.Screen name="Profile" component={Profile} />
                 <Tab.Screen name="Leaderboard" component={Leaderboard} />
             </Tab.Navigator>
