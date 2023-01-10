85de13679f10e06e0b558b039ba67b6bdb7207c2
James Ocana <ojam1996@gmail.com>
2023-01-09 19:12:12 -0500

    added icons to navbar

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,7 @@ import Profile from "./Profile";
 import Leaderboard from "./Leaderboard";
 import Home from "./Home";
 import { Feather } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons'; 
 
 const Tab = createBottomTabNavigator();
 
@@ -16,14 +17,30 @@ const Main = () => {
                 name="Home" 
                 component={Home} 
                 options={{
-                    tabBarLabel: 'Waffle',
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <Feather name="home" color={color} size={size} />
+                        )
+                    }}
+                />
+                <Tab.Screen 
+                name="Profile" 
+                component={Profile}
+                options={{
                     tabBarIcon: ({ color, size }) => (
                         <Feather name="user" color={color} size={size} />
-                      )
-                  }}
+                        )
+                    }}
                 />
-                <Tab.Screen name="Profile" component={Profile} />
-                <Tab.Screen name="Leaderboard" component={Leaderboard} />
+                <Tab.Screen 
+                name="Leaderboard" 
+                component={Leaderboard} 
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="leaderboard" color={color} size={size} />
+                        )
+                    }}
+                />
             </Tab.Navigator>
         </>
     )
