e268abf6bd4b8155a084045a13d7acef37fbe18c
James Ocana <ojam1996@gmail.com>
2023-01-17 01:51:33 -0500

    progress bar timer updating on setInterval

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet, TextInput, Button } from "react-native";
 import Hello from "./Hello";
+import * as Progress from 'react-native-progress';
 
 let x = 0;
 
@@ -8,43 +9,52 @@ const Timer = () => {
     const [min, setMin] = useState(0)
     const [sec, setSec] = useState(0)
     const [mil, setMil] = useState(0)
+    const [progress, setProgress] = useState(0)
     const [time, setTime] = useState(0);
     const [modalVisible, setModalVisible] = useState(false);
     const [isActive, setActive] = useState(false)
     const [disabled, setDisabled] = useState(false)
 
-    const transformMinutes=()=>{
-        const convertedValue= (time / 60000);
-        const formattedValue=(convertedValue)
+    const transformMinutes = () => {
+        const convertedValue = (time / 60000);
+        const formattedValue = (convertedValue)
         return formattedValue
     }
-    const transformSeconds=()=>{
-        const convertedValue= (time / 1000);
-        const formattedValue=(convertedValue)
+    const transformSeconds = () => {
+        const convertedValue = (time / 1000);
+        const formattedValue = (convertedValue)
         return formattedValue
     }
-    const transformMilliseconds=()=>{
-        const convertedValue= (time / 10);
-        const formattedValue=(convertedValue)
+    const transformMilliseconds = () => {
+        const convertedValue = (time / 10);
+        const formattedValue = (convertedValue)
         return formattedValue
     }
-    
+
     const handleTimeChange = () => {
-        setTime(min*60000 + sec*1000 + mil*10);
+        setTime(min * 60000 + sec * 1000 + mil * 10);
     }
 
     function add() {
         x++;
+        setProgress((progress) => progress+0.1)
+        console.log(progress)
         console.log(x)
         if (x >= 10) {
             setActive(false);
             setDisabled(false);
             setModalVisible(true)
-            x=0;
+            x = 0;
         }
     }
-    
+
+    // const clickButton = () => {
+    //     setProgress(progress+0.1)
+    //     console.log(progress)
+    // }
+
     useEffect(something, [isActive])
+    //useEffect(clickButton, [x])
 
     function something() {
         let intervalId;
@@ -53,37 +63,44 @@ const Timer = () => {
         return () => clearInterval(intervalId)
     }
 
-    const startTimer = (event) => {
+    const startTimer = () => {
         setActive(true);
         setDisabled(true);
         something();
     }
-    
+
     const stopTimer = () => {
         setActive(false)
         setDisabled(false)
     }
 
-    return(
+    return (
         <View>
             <Text>
-                <TextInput style={styles.input} onSubmitEditing={handleTimeChange} onChangeText={setMin} keyboardType={"numeric"} value={min}/>
-                :<TextInput style={styles.input} onSubmitEditing={handleTimeChange} onChangeText={setSec} keyboardType={"numeric"} value={sec}/>
-                :<TextInput style={styles.input} onSubmitEditing={handleTimeChange} onChangeText={setMil} keyboardType={"numeric"} value={mil}/>
+                <TextInput style={styles.input} onSubmitEditing={handleTimeChange} onChangeText={setMin} keyboardType={"numeric"} value={min} />
+                :<TextInput style={styles.input} onSubmitEditing={handleTimeChange} onChangeText={setSec} keyboardType={"numeric"} value={sec} />
+                :<TextInput style={styles.input} onSubmitEditing={handleTimeChange} onChangeText={setMil} keyboardType={"numeric"} value={mil} />
             </Text>
-            <Button title="Start" disabled={disabled} onPress={startTimer}/>
-            <Button title="Stop" onPress={stopTimer}/>
-            <Hello modalVisible={modalVisible} setModalVisible={setModalVisible}/>
+            <Button title="Start" disabled={disabled} onPress={startTimer} />
+            <Button title="Stop" onPress={stopTimer} />
+            <Hello modalVisible={modalVisible} setModalVisible={setModalVisible} />
+            <Progress.Bar 
+                progress={progress} 
+                width={200} 
+                borderWidth={0}
+                animated={true}
+                />
+            {/* <Button onPress={clickButton} title="CLICK ME"/> */}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     input: {
         fontSize: 14,
@@ -92,7 +109,12 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         maxWidth: 40,
         width: 40,
+    },
+    timer: {
+        height: 10,
+        width: 100,
+        backgroundColor: "red",
     }
-  });
+});
 
 export default Timer;
